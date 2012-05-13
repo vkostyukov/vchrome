5a6608d3fa4c289a4999b11a6d8f149ac8bb012e
Vladimir Kostyukov <vladimir.kostukov@gmail.com>
2012-05-13 21:55:36 +0700

    latest version

--- js/vchrome.js
+++ js/vchrome.js
@@ -24,14 +24,42 @@
 
 			path = path.split(",")[0];
 
+			// add post processing
 			title = title.substring(0, title.length - 1);
 			title = title.replace(/\"/gi, "&quot;");
 
-			var image = chrome.extension.getURL("img/download.png");
-			var style = "width:16px;height:16px;float:right;padding-right:2px;margin-top:2px;background:url('" + image + "') no-repeat;";
-			var button = "<a title=\"Download as " + title + ".mp3\" href=\"" + path + "\" download=\"" + title + ".mp3\" style=\"" + style + "\"></a>";
+			var blueImage = chrome.extension.getURL("img/disk-blue.png");
+			var blueStyle = "width:16px;height:16px;float:left;padding-right:2px;background:url('" + blueImage + "') no-repeat;";
+			var blueButton = "<a title=\"Download as " + title + ".mp3\" href=\"" + path + "\" download=\"" + title + ".mp3\" style=\"" + blueStyle + "\"></a>";
 
-			$("table tbody tr td.info", this).after("<td>" + button + "</td>");
+			var infoWidth = $("td.info", this).css("width");
+
+			$("td.info", this).before("<td>" + blueButton + "</td>");
+
+			var titleHandler = function(index, value) {
+				return value.substring(0, value.length - 2) - 16 + "px";
+			};
+
+			var infoHandler = function(index, value) {
+				if (value != infoWidth) return value;
+				return value.substring(0, value.length - 2) - 24 + "px";
+			};
+
+			$("td.info .audio_title_wrap", this).css("width", titleHandler);
+			$("td.info .title_wrap", this).css("width", titleHandler);
+
+			$("td.info", this).css("width", infoHandler);
+
+			$("td.play_btn", this).bind("click", function() {
+				var whiteImage = chrome.extension.getURL("img/disk-white10.png");
+				var whiteStyle = "width:12px;height:12px;float:left;margin-top:12px;padding-right:4px;margin-left:4px;background:url('" + whiteImage + "') no-repeat;";
+				var whiteButton = "<a onmouseover=\"showTooltip(this, {content: '&lt;div class=gp_tip_text&gt;Download as "+ title + " .mp3&lt;/div&gt;',className:'gp_tip',shift:[4,12]});\" href=\"" + path + "\" download=\"" + title + ".mp3\" style=\"" + whiteStyle + "\"></a>";
+
+				$("#audio_global #gp_large .play_btn").after(whiteButton);
+			});
+
+			$("#gp_audio_info_large").css("width", "310px");
+			$("#gp_duration.duration").css("right", "20px");
 
 			frozen = false;
 		}
@@ -41,7 +69,7 @@
 		if (methods[method]) {
 			return this.each(methods[method]);
 		} else {
-			$.error("Method " +  method + " does not exist on jQuery.vkontakte");
+			$.error("Method " +  method + " does not exist on jQuery.vchrome");
 		}
 	};
 })(jQuery);
