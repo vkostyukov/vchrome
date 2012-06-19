ed629d44e83ecd909d791d1182b80c02d7d36da8
Vladimir Kostyukov <vladimir.kostukov@gmail.com>
2012-06-19 22:02:58 +0700

    working version

--- js/vchrome.js
+++ js/vchrome.js
@@ -1,7 +1,7 @@
 /*!
  * VChrome is Chrome Extension for VKontakte (http://vk.com).
  *
- * Version: 0.3.0 (Jun 2012)
+ * Version: 0.4.0 (Jun 2012)
  *
  * Copyright 2012, Vladimir Kostyukov (http://vkostyukov.ru)
  * License: http://www.apache.org/licenses/LICENSE-2.0.html
@@ -12,31 +12,43 @@
 (function($) {
 	var frozen = false;
 
+	var checksum = function (string) {
+		var chk = 0x123456789;
+		for (var i = 0; i < string.length; i++) {
+			chk += (string.charCodeAt(i) * i);
+		}
+		return chk;
+	};
+
 	var methods = {
 		audio: function() {
-			if (frozen || $(this).attr("vch-processed") == "true") return;
-
+			if (frozen) return;
 			frozen = true;
-			$(this).attr("vch-processed", "true");
-
-			var title = $("td.info .title_wrap", this).text()
-				|| $("td.info .audio_title_wrap", this).text();
-			var path = $("table tbody tr td.play_btn input", this).attr("value")
-				|| $("table tbody tr td input", this).attr("value");
-
-			path = path.split(",")[0];
-
-			// add post processing
-			title = title.substring(0, title.length - 1);
-			title = title.replace(/\"/gi, "&quot;");
-
-			var blueImage = chrome.extension.getURL("img/disk-blue.png");
-			var blueStyle = "width:16px;height:16px;float:left;padding-right:2px;background:url('" + blueImage + "') no-repeat;";
-			var blueButton = "<a title=\"Download as " + title + ".mp3\" href=\"" + path + "\" download=\"" + title + ".mp3\" style=\"" + blueStyle + "\"></a>";
 
 			var infoWidth = $("td.info", this).css("width");
+			var chk = checksum(infoWidth);
+			if (chk == $(this).attr("vch-checksum")) {
+				frozen = false;
+				return;
+			}
 
-			$("td.info", this).before("<td>" + blueButton + "</td>");
+			if ($(this).attr("vch-checksum") == undefined) {
+				var title = $("td.info .title_wrap", this).text()
+					|| $("td.info .audio_title_wrap", this).text();
+				var path = $("table tbody tr td.play_btn input", this).attr("value")
+					|| $("table tbody tr td input", this).attr("value");
+
+				path = path.split(",")[0];
+
+				title = title.replace(/\s+$/, "");
+				title = title.replace(/\"/gi, "");
+
+				var blueImage = chrome.extension.getURL("img/disk-blue.png");
+				var blueStyle = "width:16px;height:16px;float:left;padding-right:2px;background:url('" + blueImage + "') no-repeat;";
+				var blueButton = "<a title=\"Download as " + title + ".mp3\" href=\"" + path + "\" download=\"" + title + ".mp3\" style=\"" + blueStyle + "\"></a>";
+
+				$("td.info", this).before("<td>" + blueButton + "</td>");
+			}
 
 			var titleHandler = function(index, value) {
 				return value.substring(0, value.length - 2) - 16 + "px";
@@ -50,18 +62,19 @@
 			$("td.info .audio_title_wrap", this).css("width", titleHandler);
 			$("td.info .title_wrap", this).css("width", titleHandler);
 
-			$("td.info", this).css("width", infoHandler);
+			$(this).attr("vch-checksum", chk);
 
-			$("td.play_btn", this).bind("click", function() {
-				var whiteImage = chrome.extension.getURL("img/disk-white.png");
-				var whiteStyle = "width:12px;height:12px;float:left;margin-top:12px;padding-right:4px;margin-left:4px;background:url('" + whiteImage + "') no-repeat;";
-				var whiteButton = "<a onmouseover=\"showTooltip(this, {content: '&lt;div class=gp_tip_text&gt;Download as "+ title + " .mp3&lt;/div&gt;',className:'gp_tip',shift:[4,12]});\" href=\"" + path + "\" download=\"" + title + ".mp3\" style=\"" + whiteStyle + "\"></a>";
-
-				$("#audio_global #gp_large .play_btn").after(whiteButton);
-			});
-
-			$("#gp_audio_info_large").css("width", "310px");
-			$("#gp_duration.duration").css("right", "20px");
+			// TODO: add support fo white button in version 0.5
+//			$("td.play_btn", this).bind("click", function() {
+//				var whiteImage = chrome.extension.getURL("img/disk-white.png");
+//				var whiteStyle = "width:12px;height:12px;float:left;margin-top:12px;padding-right:4px;margin-left:4px;background:url('" + whiteImage + "') no-repeat;";
+//				var whiteButton = "<a onmouseover=\"showTooltip(this, {content: '&lt;div class=gp_tip_text&gt;Download as "+ title + " .mp3&lt;/div&gt;',className:'gp_tip',shift:[4,12]});\" href=\"" + path + "\" download=\"" + title + ".mp3\" style=\"" + whiteStyle + "\"></a>";
+//
+//				$("#audio_global #gp_large .play_btn").after(whiteButton);
+//			});
+//
+//			$("#gp_audio_info_large").css("width", "310px");
+//			$("#gp_duration.duration").css("right", "20px");
 
 			frozen = false;
 		}
@@ -71,7 +84,7 @@
 		if (methods[method]) {
 			return this.each(methods[method]);
 		} else {
-			$.error("Method " +  method + " does not exist on jQuery.vchrome");
+			$.error("Method " +  method + " doesn't exist on jQuery.vchrome");
 		}
 	};
 })(jQuery);
